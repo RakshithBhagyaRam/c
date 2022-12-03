03afe601995cbacb0050c92b83d0909037dc6450
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-04 01:10:46 +0530

    input
    accepting input values from user

---
+++ basics/input.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main
+{
+  // syntax:-  scanf("format specifiers",&variable_name);
+  
+  
+  /*  format specifiers are :-
+  
+      %d to accept input of integers.
+
+      %ld to  accept input of long integers
+
+      %lld to accept input of long long integers
+
+      %f to accept input of real number.
+
+      %c to accept input of character types.
+
+      %s to accept input of a string.         */
+  
+  
+  scanf("%d",&int_variable); //to take input for integer variable  ex. 1, 2, 3, 4, ....
+  
+  
+  scanf("%f",&float_variable); //to take input for float variable ex. 10.4, 1.5, 3.142, ....
+  
+  
+  scanf("%c",&char_variable); //to take input for character variable  ex. a, b, c, d, ....
+  
+  
+  scanf("%s",&string_variable); //to take input for string variable ex. name, address, text, ....
+  
+  
+}
