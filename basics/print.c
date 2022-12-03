cd55c850741e66e92e4534ffd2b21fea9b4e109c
Rakshith BR <112625689+RakshithBhagyaRam@users.noreply.github.com>
2022-12-03 23:23:17 +0530

    print

--- basics/print.c
+++ basics/print.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-void main{
-printf("Type the message to be displayed")
+void main
+{
+  printf("Type the message to be displayed")
+  
+  
+  printf("%d",int_variable) //to print the values of integer variable  ex. 1, 2, 3, 4, ....
+  
+  
+  printf("%f",float_variable) //to print the values of float variable ex. 10.4, 1.5, 3.142, ....
+  
+  
+  printf("%c",char_variable) //to print the value of character variable  ex. a, b, c, d, ....
+  
+  
+  printf("%s",string_variable) //to print the values of string variable ex. name, address, text, ....
+  
+  
 }
+
+
+
